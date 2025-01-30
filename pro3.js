243084a3b9b15685cd1c62bd24ad4687aa06b832
ArvindiGit <arvindprajapati2569@gmail.com>
2025-01-30 14:54:54 +0530

    Revert "pro2 14:30"
    This reverts commit 70fae82c2356373990b4ea1b19806147dda66697.
    
    # Conflicts:
    #	Pro2.js
    pro3 14:54

---
+++ pro3.js
@@ -0,0 +1,12 @@
+const pro3 = () => {
+  return (
+    <div>
+        lf laf <a href=""></a>
+      pro3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ea
+      obcaecati fugit exercitationem repellat quo molestiae pariatur eos soluta
+      quis ipsum debitis, harum eius laboriosam rem ut laborum quibusdam?
+      Distinctio.
+    </div>
+  );
+};
+export default pro3;
