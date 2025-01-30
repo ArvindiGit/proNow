855e40fdc7ea8b2c1ce65ee3dd25498592265992
ArvindiGit <arvindprajapati2569@gmail.com>
2025-01-30 15:01:15 +0530

    pro2 15:01

--- Pro2.js
+++ Pro2.js
@@ -1,7 +1,10 @@
 // Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga impedit dolorem dicta laudantium non praesentium maxime? Dolorem dolor laboriosam assumenda similique provident, autem dolores. Eius culpa voluptatem praesentium perferendis quis.
 const Pro2 = () => {
   return (
-    <div>Pro2</div>
+    <div>Pro2
+
+<div></div>
+    </div>
   )
 }
 export default Pro2
